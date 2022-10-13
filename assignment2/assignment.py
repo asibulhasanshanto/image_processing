2a02e9907f10571aed2efb86308db4213a8e1601
Md Asibul Hasan Shanto <48201478+asibulhasanshanto@users.noreply.github.com>
2022-10-13 16:05:01 +0600

    assignment    2 done

--- assignment2/assignment.py
+++ assignment2/assignment.py
@@ -5,23 +5,28 @@ import numpy as np
 import math
 
 def main():
-    img_path = './image.jpg'
+    img_path = './whiteFlower.jpg'
     rgb_image =  plt.imread(img_path)
     print(rgb_image.shape)
 
     grayscale_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2GRAY)
     print(grayscale_image.shape)
 
+    # take the shape of grayscale image and create empty images
     i,j = grayscale_image.shape
     processed_image1 = np.zeros((i,j), dtype = np.uint8)
     processed_image2 = np.zeros((i,j), dtype = np.uint8)
     processed_image3 = np.zeros((i,j), dtype = np.uint8)
+    processed_image4 = np.zeros((i,j), dtype = np.uint8)
 
+    # take threshold values and other parameters
     T1=100
     T2=200
-    c1 = 255/math.log(256)
-    c2 = math.log(255)/255
+    c =2
+    p=5
     epsilon =  0.0000001
+
+    # process the image
     for x in range(i):
         for y in range(j):
             if grayscale_image[x,y]>=T1 and grayscale_image[x,y]<=T2:
@@ -34,26 +39,27 @@ def main():
             else:
                 processed_image2[x,y] = grayscale_image[x,y]
             
-            processed_image3[x,y] = c1 * math.log(grayscale_image[x,y]+1)
+            processed_image3[x,y] =  c * np.log(1+grayscale_image[x,y])
+            processed_image4[x,y] = c * pow((grayscale_image[x,y] + epsilon),p)
     
-    image_set=[rgb_image, grayscale_image, processed_image1, processed_image2, processed_image3]
-    title_set=['RGB', 'Grayscale', 'Processed1', 'Processed2', 'Processed3']
+    image_set=[rgb_image, grayscale_image, processed_image1, processed_image2, processed_image3, processed_image4]
+    title_set=['RGB', 'Grayscale', 'Processed 1', 'Processed 2', 'Processed 3', 'Processed 4']
     plot_img(image_set, title_set)
 
 def plot_img(image_set, title_set):
     n = len(image_set)
-    plt.figure(figsize = (20, 20))
     for i in range(n):
+        plt.figure(figsize = (10, 15))
         img = image_set[i]
         ch = len(img.shape)
 
-        plt.subplot( 2, 3, i + 1)
+        # plt.subplot( 2, 3, i + 1)
         if (ch == 3):
             plt.imshow(image_set[i])
         else:
             plt.imshow(image_set[i], cmap = 'gray')
         plt.title(title_set[i])
-    plt.savefig('./image_processed.jpg')
+        plt.savefig('./'+title_set[i]+'.jpg')
 
 if __name__ == '__main__':
     main()
