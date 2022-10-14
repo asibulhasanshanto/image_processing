3f0bb5855017e2a520472ce00618dc96958aaee1
Md Asibul Hasan Shanto <48201478+asibulhasanshanto@users.noreply.github.com>
2022-10-14 11:40:27 +0600

    assignment3 done

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -8,23 +8,23 @@ def main():
 
     grayscale_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2GRAY)
 
+    # creating kernels
     kernel1 = np.ones((3, 3), dtype = np.float32)*25/100
-    # print(kernel1)
-
-    kernelBlur = np.array([[0,0.4,0],[0.4,-0.9,0.4],[0,0.4,0]])
-    kernel3 = np.ones((3, 3), dtype = np.float32)*-0.5
-    kernel3[1,1]=4
-
+    kernel2 = np.array([[0,0.4,0],[0.4,-0.9,0.4],[0,0.4,0]])
+    kernel3 = np.ones((3, 3), dtype = np.float32)*-0.5;kernel3[1,1]=4
     kernel4 = np.array([[3,4,5],[0,0,0],[-3,-4,-5]])
     kernel5 = np.array([[-3,-2,-1],[-1,1,1],[1,2,3]])
-    kernel6 = np.array([[-1,0,-1],[0,8,0],[-1,0,-1]])
+    kernel6 = np.array([[-1,0,-1],[0,6,0],[-1,0,-1]])
 
-    kernels = [kernel1,kernelBlur,kernel3,kernel4,kernel5,kernel6]
-    kernelNames=['Kernel1','kernel blur','kernel3','kernel4','kernel5','kernel6']
+    kernels = [kernel1,kernel2,kernel3,kernel4,kernel5,kernel6]
+
+    kernelNames=['Kernel 1','kernel 2','kernel 3','kernel 4','kernel 5','kernel 6']
     processed_images = [rgb_image,grayscale_image]
     processed_images_titlels = ['RGB Image','Grayscale Image']
 
     c = 0
+
+    # process the images
     for kernel in kernels:
         processed_image = cv2.filter2D(grayscale_image, -1, kernel)
         processed_images.append(processed_image)
@@ -35,18 +35,18 @@ def main():
 
 def plot_images(images,titles):
     n = len(images)
-    plt.figure(figsize = (20, 20))
     for i in range(n):
+        plt.figure(figsize = (15, 10))
         img = images[i]
         ch = len(img.shape)
 
-        plt.subplot(3, 3, i + 1)
+        # plt.subplot(3, 3, i + 1)
         if (ch == 3):
             plt.imshow(images[i])
         else:
             plt.imshow(images[i], cmap = 'gray')
         plt.title(titles[i])
-    plt.savefig('./output')
+        plt.savefig('./'+titles[i]+'.png')
     # plt.show()
 if __name__ == '__main__':
     main()
