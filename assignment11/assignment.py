a0ae4fddba2e485652d2e1b3a7ff18140408efb5
Md Asibul Hasan Shanto <48201478+asibulhasanshanto@users.noreply.github.com>
2022-10-15 22:58:29 +0600

    assignment 11 completed

--- assignment11/assignment.py
+++ assignment11/assignment.py
@@ -2,42 +2,53 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# mehedis code
-
 def main():
-    rgbImg = plt.imread('dahlia.jpg')
+    rgbImg = plt.imread('sunflower.jpg')
     print(rgbImg.shape)
+    
+    # take the original image
     gray =  cv2.cvtColor(rgbImg,cv2.COLOR_RGB2GRAY)
-    ftImg = np.fft.fft2(gray)
-    centeredfti_img = np.fft.fftshift(ftImg)
-    magnitude_spectrum = 100 * np.log(np.abs(ftImg))
-    centered_magnitude_spectrum = 100 * np.log(np.abs(centeredfti_img))
+
+    # apply fast fourier transform
+    image_in_frequency_domain = np.fft.fft2(gray)
+    # shift the zero-frequency component to the center of the spectrum
+    centered_f_img = np.fft.fftshift(image_in_frequency_domain)
+    magnitude_spectrum = 100 * np.log(np.abs(image_in_frequency_domain))
+    centered_magnitude_spectrum = 100 * np.log(np.abs(centered_f_img))
     
+    # prepare the filter
     r , c = gray.shape
-    m , n = r//2 , c//2
-    black_img = np.zeros((r,c),dtype=np.uint8)
-    white_img = np.ones((r,c),dtype=np.uint8)
+    shell_image = np.ones((r,c),dtype=np.uint8)
     
-    filter1 = cv2.circle(black_img.copy(),(m,n),25,(255,255,255),-1)
-    filter2 = cv2.circle(white_img.copy(),(m,n),25,(0,0,0),-1)
-    filter3 = cv2.line(black_img.copy(),(m,0),(m,c),(255,255,255),9)
-    filter4 = cv2.line(white_img.copy(),(0,n),(r,n),(0,0,0),9)
+    # filter = cv2.circle(shell_image,(int(c/2),int(r/2)),30,(0,0,0),-1) #high pass filter
+    # filter = cv2.circle(shell_image,(int(c/2),int(r/2)),30,(255,255,255),-1)#low pass filter
+    filter = cv2.line(shell_image,(0,int(r/2)),(c,int(r/2)),(0,0,0),9) # high pass line
+    # filter = cv2.line(shell_image,(int(c/2),0),(int(c/2),c),(255,255,255),9) #low pass line
+
+    # apply the filter
+    filter_applied_f_img = centered_f_img * filter
+
+    # inverse fourier transform
+    filtered_img = np.abs(np.fft.ifft2(filter_applied_f_img))   
+
+
+    # filter2 = cv2.circle(white_img.copy(),(m,n),25,(0,0,0),-1)
+    # filter3 = cv2.line(white_img.copy(),(m,0),(m,c),(255,255,255),9)
+    # filter4 = cv2.line(white_img.copy(),(0,n),(r,n),(0,0,0),9)
 
     
-    ftimg_gf = centeredfti_img * filter4
-    filtered_img = np.abs(np.fft.ifft2(ftimg_gf))
 
     
-    img_set = [rgbImg, gray, magnitude_spectrum, centered_magnitude_spectrum, filter4, filtered_img]
-    title_set = ['RGB', 'Gray', 'FFT2', 'Centered FFT2', 'Filter', 'Filtered Img']
+    img_set = [magnitude_spectrum, centered_magnitude_spectrum, filter, filtered_img]
+    title_set = ['FFT2 of image', 'Centered FFT2 of image', 'High pass line filter', 'Filtered Img']
     
     plot_img(img_set,title_set)
     
 def plot_img(img_set, title_set):		
-    plt.figure(figsize = (20, 20))
+    plt.figure(figsize = (10,8))
     n = len(img_set)
     for i in range(n):
-        plt.subplot(2, 3, i + 1)
+        plt.subplot(2, 2, i + 1)
         plt.title(title_set[i])
         img = img_set[i]
         ch = len(img.shape)
@@ -45,7 +56,8 @@ def plot_img(img_set, title_set):
             plt.imshow(img, cmap = 'gray')
         else:
             plt.imshow(img)			
-    plt.show()
+    plt.savefig('high_pass_line_filter.png')
+    # plt.show()
 
 
 
